0b3c5b81759701f284297bbdd3366f42b0826dae
Raymond Kroon <raymond@k3n.nl>
2019-11-24 13:38:52 +0100

    routed iptv new version, inspired by https://github.com/nielsvr/UnifiKPN

---
+++ README.md
@@ -0,0 +1,5 @@
+# Setup IPTV
+
+- Copy config.gateway.json to cloudkey `/srv/unifi/data/sites/default/`
+- Copy `tvroute.sh` to USG `/config/scripts` (home folder + move + make excutable)
+- Force provision USG
--- tvroute.sh
+++ tvroute.sh
@@ -1,23 +1,20 @@
 #!/bin/vbash
-  
-# configured static route target
-CUR_IP=\$(cat /config/config.boot | grep 213.75.112.0/21 -A1 | grep next-hop | awk '{ print \$2}');
-  
-# static route target
-NEW_IP=\$(cat /var/run/dhclient_eth0.4_lease | grep new_routers | awk -F= '{print \$2}' | tr -d \');
-  
-if [ "\$CUR_IP" = "\$NEW_IP" ]; then
-   echo Addresses the same
-   exit 1;
-fi
-  
+
 source /opt/vyatta/etc/functions/script-template
-  
+
+CUR_IP=$(cat /config/config.boot | grep 213.75.112.0/21 -A1 | grep next-hop | awk '{ print $2}');
+
+NEW_IP=$(run show dhcp client leases | grep router | awk '{ print $3 }');
+iptv_static=$(echo "set protocols static route 213.75.112.0/21 next-hop $NEW_IP")
+
+if [ "$CUR_IP" = "$NEW_IP" ]; then
+echo Addresses the same
+exit 1; fi
+
 configure
-delete protocols static route 213.75.112.0/21 next-hop \$CUR_IP
-set protocols static route 213.75.112.0/21 next-hop \$NEW_IP
+delete protocols static route 213.75.112.0/21
+eval $iptv_static
 commit
 save
 exit
-  
 restart igmp-proxy
